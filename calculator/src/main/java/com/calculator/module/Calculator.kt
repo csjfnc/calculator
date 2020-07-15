344edd3e90adabfea6fb5fd874a108d5f2b19c5c
Francisco Santos <csj.fnc@gmail.com>
2020-07-15 17:09:10 -0300

    calculator

---
+++ calculator/src/main/java/com/calculator/module/Calculator.kt
@@ -0,0 +1,20 @@
+package com.calculator.module
+
+class Calculator(val number1: Double, val number2: Double ){
+
+    fun addition(): Double{
+        return number1 + number2;
+    }
+
+    fun subtraction(): Double{
+        return number1 - number2;
+    }
+
+    fun multiplication(): Double{
+        return number1 - number2;
+    }
+
+    fun division(): Double{
+        return number1 - number2;
+    }
+}
